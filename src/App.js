d1a1bb009e0d51c77b52fc28389af14d4176c3e3
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2020-09-14 08:01:41 -0700

    feat(layout): adds rough layout of page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
+/** @jsx jsx */
+// ^ enables emotion `css` syntax to be used
 import React from 'react';
+import { Layout, Row, Col } from 'antd';
+import { css, jsx } from '@emotion/core';
+
 import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Header
+        css={css`
+          top: 0;
+          position: sticky;
+          z-index: 1;
+          width: 100%;
+        `}
+      >
+        <h2
+          css={css`
+            color: white;
+          `}
         >
-          Learn React
-        </a>
-      </header>
+          Scroll to find an interesting picture, click it to learn more!
+        </h2>
+      </Header>
+      <Content>
+        <Row>
+          <Col span={12}>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Let's list the images here</p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </Col>
+          <Col span={12}>
+            <div
+              css={css`
+                position: sticky;
+                top: 64px;
+              `}
+            >
+              Details
+            </div>
+          </Col>
+        </Row>
+      </Content>
+      <Footer>a /pics browser</Footer>
     </div>
   );
 }
